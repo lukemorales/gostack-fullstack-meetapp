4246815daf36143d01e2a4e68dbd6a52c50ec6a0
Luke Morales <lukemorales@live.com>
2019-08-25 20:03:56 -0300

    feat: User Model and Migration

---
+++ backend/src/app/models/User.js
@@ -0,0 +1,17 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
