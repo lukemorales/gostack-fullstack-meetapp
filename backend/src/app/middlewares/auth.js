c017ba169b2fcff69a5c1d59063201487dac8c1b
Luke Morales <lukemorales@live.com>
2019-08-25 21:24:35 -0300

    feat: Authentication Middleware

---
+++ backend/src/app/middlewares/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.json(401).json({ error: 'Token not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userID = decoded.id;
+
+    return next();
+  } catch (error) {
+    return res.json(401).json({ error: 'Invalid token' });
+  }
+
+  return next();
+};
