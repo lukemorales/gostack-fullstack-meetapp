8d7f8369ae920337a66c691aa578741cb22d73ea
Luke Morales <lukemorales@live.com>
2019-09-01 19:00:30 -0300

    feat: Subscription Screen

---
+++ mobile/src/screens/Subscriptions/Subscriptions.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
+import { withNavigationFocus } from 'react-navigation';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '~/services/api';
+
+import Header from '~/components/Header';
+import Loading from '~/components/Loading';
+import Meetup from '~/components/Meetup';
+
+import { Container, List, Empty, EmptyText } from './Subscriptions_Styles';
+
+function Subscriptions({ isFocused }) {
+  const [subscriptions, setSubscriptions] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function loadSubscriptions() {
+    setLoading(true);
+
+    const response = await api.get('subscriptions');
+
+    setSubscriptions(response.data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    if (isFocused) {
+      loadSubscriptions();
+    }
+  }, [isFocused]);
+
+  async function handleCancel(id) {
+    try {
+      await api.delete(`subscriptions/${id}`);
+      Alert.alert('Sucesso', 'Sua inscrição foi cancelada!');
+      loadSubscriptions();
+    } catch (error) {
+      const message = error.response.data.error;
+      Alert.alert('Error', message);
+    }
+  }
+
+  return (
+    <>
+      <Header />
+      <Container>
+        {loading && <Loading />}
+
+        {!loading &&
+          (subscriptions.length ? (
+            <List
+              data={subscriptions}
+              keyExtractor={item => String(item.id)}
+              renderItem={({ item }) => (
+                <Meetup
+                  data={item.meetup}
+                  handleCancel={() => handleCancel(item.id)}
+                />
+              )}
+            />
+          ) : (
+            <Empty>
+              <Icon name="event-busy" size={45} color="rgba(0, 0, 0, .15)" />
+              <EmptyText>You haven't subscribed to any meetups yet.</EmptyText>
+            </Empty>
+          ))}
+      </Container>
+    </>
+  );
+}
+Subscriptions.navigationOptions = {
+  tabBarLabel: 'Inscrições',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
+export default withNavigationFocus(Subscriptions);
---
+++ mobile/src/screens/Subscriptions/Subscriptions_Styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  padding-top: 30px;
+`;
+
+export const Empty = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+export const EmptyText = styled.Text`
+  font-size: 14px;
+  color: #555;
+  margin-top: 15px;
+`;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})``;
