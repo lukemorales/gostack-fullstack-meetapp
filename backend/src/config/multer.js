3d656d3c9aeaff646c106e406e547341b6fe9eb9
Luke Morales <lukemorales@live.com>
2019-08-31 22:53:20 -0300

    fix: Fixed Wrong Path to 'tmp' Folder

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -4,7 +4,7 @@ import { extname, resolve } from 'path';
 
 export default {
   storage: multer.diskStorage({
-    destination: resolve(__dirname, '..', '..', '..', 'tmp', 'uploads'),
+    destination: resolve(__dirname, '..', '..', 'tmp', 'uploads'),
     filename: (req, file, cb) => {
       crypto.randomBytes(16, (err, res) => {
         if (err) return cb(err);
